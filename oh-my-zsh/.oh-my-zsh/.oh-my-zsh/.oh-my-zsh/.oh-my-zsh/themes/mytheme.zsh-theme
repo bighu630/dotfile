465be7681e34751e8c7a7e924e889e21a427c789
ivhu <ivhu@foxmail.com>
2024-08-09 16:18:39 +0800

    update

--- oh-my-zsh/.oh-my-zsh/.oh-my-zsh/.oh-my-zsh/.oh-my-zsh/themes/mytheme.zsh-theme
+++
@@ -1,7 +0,0 @@
-PROMPT="%{$fg[cyan]%}%c%{$reset_color%} %(?:%{$fg_bold[green]%}%1{➜%}:%{$fg_bold[red]%}%1{➜%})"
-PROMPT+=' $(git_prompt_info)'
-
-ZSH_THEME_GIT_PROMPT_PREFIX="%{$fg_bold[blue]%}git:(%{$fg[yellow]%}"
-ZSH_THEME_GIT_PROMPT_SUFFIX="%{$reset_color%} "
-ZSH_THEME_GIT_PROMPT_DIRTY="%{$fg[blue]%}) %{$fg[yellow]%}%1{✗%}"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg[blue]%})"
--- updata.sh
+++ updata.sh
@@ -1,14 +1,14 @@
 git pull
 cp -rf $HOME/.zshrc zshrc
 cp -rf $HOME/.zshrc-alias zshrc-alias
-cp -rf $HOME/.oh-my-zsh oh-my-zsh
-cp -rf $HOME/.vim vim
-cp -rf $HOME/.config/nvim nvim
-cp -rf $HOME/.config/ncmpcpp config/
-cp -rf $HOME/.config/mpd config/
-cp -rf $HOME/.config/rofi config/
-cp -rf $HOME/.config/ranger config/
-cp -rf $HOME/.config/picom config/
+cp -rf $HOME/.oh-my-zsh/* oh-my-zsh
+cp -rf $HOME/.vim/* vim
+cp -rf $HOME/.config/nvim/* nvim
+cp -rf $HOME/.config/ncmpcpp/* config/ncmpcpp
+cp -rf $HOME/.config/mpd/* config/mpd
+cp -rf $HOME/.config/rofi/* config/rofi
+cp -rf $HOME/.config/ranger config/ranger
+cp -rf $HOME/.config/picom config/picom
 cp -rf $HOME/.tmux.conf tmux/.tmux.conf
 cp -rf $HOME/.tmux.conf.local tmux/.tmux.conf.local
 git add *
