e4b2f1253ff60e5b636fd83738cac009d96aa41c
ivhu <ivhu@foxmail.com>
2024-08-27 17:59:11 +0800

    update .

--- updata.sh
+++ updata.sh
@@ -1,12 +1,4 @@
 git pull
-cp -rf $HOME/.config/ncmpcpp/* config/ncmpcpp
-cp -rf $HOME/.config/mpd/* config/mpd
-cp -rf $HOME/.config/rofi/* config/rofi
-cp -rf $HOME/.config/ranger config/ranger
-cp -rf $HOME/.config/picom config/picom
-cp -rf $HOME/.tmux.conf tmux/.tmux.conf
-cp -rf $HOME/.tmux.conf.local tmux/.tmux.conf.local
-
 
 #!/bin/bash
 
