e37bbd5e85f591803a47548f2656ee92eaa82f7b
ivhu <ivhu@foxmail.com>
2024-08-27 17:58:33 +0800

    update .

--- install.sh
+++ install.sh
@@ -1,30 +1,43 @@
 #!/bin/bash
 local=$(pwd)
 git pull
-###### zsh
-rm -rf $HOME/.oh-my-zsh-bak
-ln -s -b $local/zshrc $HOME/.zshrc
-ln -s -b $local/zshrc-alias $HOME/.zshrc-alias
-ln -s -b $local/oh-my-zsh $HOME/.oh-my-zsh
+
+backup_and_link() {
+    source_file=$1
+    target=$2
+    backup_suffix=".bak"
+
+    # 检查目标是否存在且不是软链接
+    if [ -e "$target" ] && [ ! -L "$target" ]; then
+        echo "Backing up $target to $target$backup_suffix"
+        mv "$target" "$target$backup_suffix"
+    fi
+
+    # 创建符号链接
+    ln -s -b "$source_file" "$target"
+}
+
+##### zsh
+backup_and_link "$local/zshrc" "$HOME/.zshrc"
+backup_and_link "$local/zshrc-alias" "$HOME/.zshrc-alias"
+backup_and_link "$local/oh-my-zsh" "$HOME/.oh-my-zsh"
 
 ##### vim
-rm -rf $HOME/.config/nvim-bak
-rm -rf $HOME/.vim-bak
-# mv $HOME/.vim $HOME/.vim-bak
-# mv $HOME/.config/nvim $HOME/.config/nvim-bak
-ln -s -b ${local}/nvim ~/.config
-ln -s -b ${local}/vim ~/.vim
+backup_and_link "$local/nvim" "$HOME/.config/nvim"
+backup_and_link "$local/vim" "$HOME/.vim"
 
 ##### config
-cp -rf config/* $HOME/.config
+backup_and_link "$local/config/mpd" "$HOME/.config/mpd"
+backup_and_link "$local/config/ncmpcpp" "$HOME/.config/ncmpcpp"
+backup_and_link "$local/config/picom" "$HOME/.config/picom"
+backup_and_link "$local/config/ranger" "$HOME/.config/ranger"
+backup_and_link "$local/config/rofi" "$HOME/.config/rofi"
+backup_and_link "$local/alacritty" "$HOME/.config/alacritty"
 
 ##### tmux
-mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
-mv $HOME/.tmux.conf.local $HOME/.tmux.conf.local.bak
-cp -rf tmux/.tmux.conf $HOME
-cp -rf tmux/.tmux.conf.local $HOME
+backup_and_link "$local/tmux/.tmux.conf.local" "$HOME/.tmux.conf.local"
+backup_and_link "$local/tmux/.tmux.conf" "$HOME/.tmux.conf"
 
-#### 杂项
-#
-cp -rf ./conkyrc $HOME/.conkyrc
-cp -rf ./autostart.sh $HOME/.autostart.sh
+##### 杂项
+backup_and_link "$local/conkyrc" "$HOME/.conkyrc"
+backup_and_link "$local/autostart.sh" "$HOME/.autostart.sh"
