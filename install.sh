b26866314813e0661ce2ad295ba496238270fdb9
ivhu <ivhu@foxmail.com>
2024-08-28 10:17:41 +0800

    update .

--- install.sh
+++ install.sh
@@ -17,6 +17,33 @@ backup_and_link() {
     ln -s -b -T "$source_file" "$target"
 }
 
+backup_and_link_dirs(){
+    source_dir=$1
+    target_dir=$2
+    backup_suffix=".bak"
+
+    # 确保目标目录存在
+    mkdir -p "$target_dir"
+
+    # 遍历原文件夹中的所有一级文件夹
+    for dir in "$source_dir"/*/; do
+        # 获取目录名
+        dir_name=$(basename "$dir")
+
+        # 目标文件夹路径
+        target="$target_dir/$dir_name"
+
+        # 检查目标是否存在且不是软链接
+        if [ -e "$target" ] && [ ! -L "$target" ]; then
+            echo "Backing up $target to $target$backup_suffix"
+            mv "$target" "$target$backup_suffix"
+        fi
+
+        # 创建符号链接
+        ln -s -f -T "$dir" "$target"
+    done
+}
+
 ##### zsh
 backup_and_link "$local/zshrc" "$HOME/.zshrc"
 backup_and_link "$local/zshrc-alias" "$HOME/.zshrc-alias"
@@ -27,12 +54,7 @@ backup_and_link "$local/nvim" "$HOME/.config/nvim"
 backup_and_link "$local/vimrc" "$HOME/.vim"
 
 ##### config
-backup_and_link "$local/config/mpd" "$HOME/.config/mpd"
-backup_and_link "$local/config/ncmpcpp" "$HOME/.config/ncmpcpp"
-backup_and_link "$local/config/picom" "$HOME/.config/picom"
-backup_and_link "$local/config/ranger" "$HOME/.config/ranger"
-backup_and_link "$local/config/rofi" "$HOME/.config/rofi"
-backup_and_link "$local/alacritty" "$HOME/.config/alacritty"
+backup_and_link_dirs "$local/config" "$HOME/.config"
 
 ##### tmux
 backup_and_link "$local/tmux/.tmux.conf.local" "$HOME/.tmux.conf.local"
