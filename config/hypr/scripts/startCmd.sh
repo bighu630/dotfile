19cb97087d79313bf809d9242a77329ae9a6f08d
ivhu <ivhu>
2024-11-13 08:26:41 +0800

    update .

--- config/hypr/scripts/startCmd.sh
+++ config/hypr/scripts/startCmd.sh
@@ -1,8 +1,24 @@
 # 不是马上执行
 sleep 1
-while true; do
-    sleep 10
+menu() {
+    while true; do
+        sleep 10
 
-    XDG_MENU_PREFIX=arch- kbuildsycoca6
-    sleep 30
-done
+        XDG_MENU_PREFIX=arch- kbuildsycoca6
+        sleep 30
+    done
+}
+
+waybarUp() {
+    while true; do
+        # 检查 waybar 是否正在运行
+        if ! pgrep waybar > /dev/null; then
+            # 如果 waybar 未运行，则启动它
+            waybar &
+        fi
+        sleep 10
+    done
+}
+
+waybarUp &
+menu
