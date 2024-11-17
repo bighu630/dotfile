5c8a06049d72dc6f26e418a3d8c69a8d076b4356
ivhu <ivhu@foxmail.com>
2024-11-17 19:44:08 +0800

    update .

--- config/waybar/scripts/warpaper.sh
+++ config/waybar/scripts/warpaper.sh
@@ -1,5 +1,5 @@
 for output in `swww query | awk -F: '{print $1}'`; do
-    IMG=$(shuf -n1 -e `cat $HOME/.config/hypr/wallpapers/bg.list`)
+    IMG=$(find ~/dotfile/config/hypr/wallpapers/bg -type f | shuf -n 1)
     echo Display $output "$IMG"
     swww img -o $output -t outer "$IMG"
 done
