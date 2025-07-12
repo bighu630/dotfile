75fecff58b177cf20de3bb3f51827f8b3803531c
ivhu <ivhu@foxmail.com>
2025-07-12 17:15:15 +0800

    chore: Update submodules

--- config/waybar/scripts/net.sh
+++ config/waybar/scripts/net.sh
@@ -23,18 +23,18 @@ if [[ $RX -lt 1024 ]]; then
         # RX="${RX}B/s"
         RX="0 B/s"
 elif [[ $RX -gt 1048576 ]]; then
-        RX=$(echo $RX | awk '{printf("%3.1f M/s",$1/1048576)}')
+        RX=$(echo $RX | awk '{printf("%3.1fM",$1/1048576)}')
 else
-        RX=$(echo $RX | awk '{printf("%4.1f K/s",$1/1024)}')
+        RX=$(echo $RX | awk '{printf("%4.1fK",$1/1024)}')
 fi
 
 if [[ $TX -lt 1024 ]]; then
         # TX="${TX}B/s"
         TX=" 0 B/s"
 elif [[ $TX -gt 1048576 ]]; then
-        TX=$(echo $TX | awk '{printf("%3.1f M/s",$1/1048576)}')
+        TX=$(echo $TX | awk '{printf("%3.1fM",$1/1048576)}')
 else
-        TX=$(echo $TX | awk '{printf("%4.1f K/s",$1/1024)}')
+        TX=$(echo $TX | awk '{printf("%4.1fK",$1/1024)}')
 fi
 #echo -e "🌈↓$RX | ↑$TX "
 echo -e "↓$RX ↑$TX"
